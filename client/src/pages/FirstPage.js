fb7ba8a346969c8d3aa183d9f0e09f5291b68ec2
biswajeetbiswal <biswajeetbiswal22@gmail.com>
2023-06-10 19:43:45 +0530

    added

---
+++ client/src/pages/FirstPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/RegiserStyles.css";
+import Layout from "./../components/Layout";
+
+
+
+
+export const FirstPage = () => {
+  return (
+    <Layout>
+        <div>FirstPage</div>
+    </Layout>
+  )
+}
