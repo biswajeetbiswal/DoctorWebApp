a18a9edc8f584bc9a0668dfc9f16469bde993b0f
biswajeetbiswal <biswajeetbiswal22@gmail.com>
2023-11-18 23:31:08 +0530

    updated

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(moragan("dev"));
 
 //routes
-app.get('/', (req, res)=> res.send("Hello World!"))
+app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
